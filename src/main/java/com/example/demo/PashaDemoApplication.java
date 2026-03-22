07772591704534f5004d75ba14799ae23d1a9746
pboro <pborovylk@gmail.com>
2026-03-22 01:04:30 +0100

    init

---
+++ src/main/java/com/example/demo/PashaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PashaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PashaDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/Rest.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class Rest {
+
+    private static final Logger logger = LoggerFactory.getLogger(Rest.class);
+
+    @GetMapping("/logs")
+    @ResponseStatus(value = HttpStatus.OK)
+    public String getAssetStatuses() {
+
+        logger.info("Test pasha App");
+        return "Test logging";
+    }
+
+}
