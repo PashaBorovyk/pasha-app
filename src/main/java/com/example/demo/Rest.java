2f6c5cebfed789f81aee71d65c9f35da5f8141ed
pboro <pborovylk@gmail.com>
2026-03-22 17:28:41 +0100

    1.3

--- src/main/java/com/example/demo/Rest.java
+++ src/main/java/com/example/demo/Rest.java
@@ -18,8 +18,8 @@ public class Rest {
     @ResponseStatus(value = HttpStatus.OK)
     public String getAssetStatuses() {
 
-        logger.info("Test pasha App 1.1");
-        return "Test logging 1.1";
+        logger.info("Test pasha App 1.3");
+        return "Test logging 1.3";
     }
 
 }
